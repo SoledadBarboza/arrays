6e80f2bda8630aa27f663bea02836f992410ae60
Sole <barboza.soledad@gmail.com>
2023-06-12 11:31:30 -0300

    Array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 var children = ["Miltón", "Lucrecia", "Federico", "Aixa"];
-var ageChilden = [2, 5, 4, 3];
+// The pop method removes the last element of an array.
+children.pop();
+
+var ageChildren = [2, 5, 4, 3];
+// The slice method returns selected elements in an array, as a new array.
+ageChildren.slice(1, 3)
+
+
 var likesChocolate = [true, true, false, true];
+//The push method adds new items to the end of an array.
+likesChocolate.push(false);
+
