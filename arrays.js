d573f69b8207c4964022f04cd5f12a26a6ac27c9
Sole <barboza.soledad@gmail.com>
2023-06-12 12:04:40 -0300

    Index position

--- arrays.js
+++ arrays.js
@@ -4,10 +4,17 @@ children.pop();
 
 var ageChildren = [2, 5, 4, 3];
 // The slice method returns selected elements in an array, as a new array.
-ageChildren.slice(1, 3)
+ageChildren.slice(1, 3);
 
 
 var likesChocolate = [true, true, false, true];
 //The push method adds new items to the end of an array.
 likesChocolate.push(false);
 
+//An array is a collection of values, organized in a specific order.
+//Each value in the array has a number.
+//counting starts with 0.
+//Example: what numerical position in the array "Lucrecia" has?
+
+var children = ["Miltón", "Lucrecia", "Federico", "Aixa"];
+console.log(children[1]);
